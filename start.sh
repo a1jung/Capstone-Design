ce1dd1f6b6e7e059143d19a013987fb2af3323c0
a1jung <162448349+a1jung@users.noreply.github.com>
2025-11-17 17:16:02 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+export OPENAI_API_KEY="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"
+uvicorn main:app --host 0.0.0.0 --port 10000 --reload
