c6480dd7e87c7e8c9db7c1921a7ccfc7e4158860
a1jung <162448349+a1jung@users.noreply.github.com>
2025-11-19 09:46:24 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# 가상환경 활성화
-source .venv/bin/activate
-
-# PORT 환경변수 사용, 기본 8000
-PORT=${PORT:-8000}
-
-# uvicorn으로 FastAPI 실행
-uvicorn main:app --host 0.0.0.0 --port $PORT --reload
+export $(cat .env | xargs)  # .env 읽기
+uvicorn main:app --host 0.0.0.0 --port $PORT
